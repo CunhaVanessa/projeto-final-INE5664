3dc3d3c0eb2f19ce9a88935ddbfeb32f5a9a2fc7
Pedro Ruschel <pruschel30@gmail.com>
2025-06-29 18:18:49 -0300

    add activation function

---
+++ src/binary_classification/activations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sigmoid(x): return 1 / (1 + np.exp(-x))
+def sigmoid_deriv(x): return sigmoid(x) * (1 - sigmoid(x))
+
+def relu(x): return np.maximum(0, x)
+def relu_deriv(x): return (x > 0).astype(float)
+
+def tanh(x): return np.tanh(x)
+def tanh_deriv(x): return 1 - np.tanh(x)**2
